6f927a3d17c2be19bff3de4fa66c8e60308742da
Salym Senyonga <salymsash@gmail.com>
2012-10-26 01:08:43 +0300

    Initial commit

---
+++ northwind.rb
@@ -0,0 +1,12 @@
+require 'active_record'
+require 'yaml'
+require 'logger'
+
+dbconfig = YAML::load(File.open('/home/lym/ruby_projects/northwind/database.yml'))
+ActiveRecord::Base.establish_connection(dbconfig)
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+class Customer < ActiveRecord::Base
+end
+
+puts Customer.count
