a33aee6cb8a2ef902a2fa8cf562ceac3ef559dd0
Salym Senyonga <salymsash@gmail.com>
2012-10-26 01:35:53 +0300

    changed home folder reference variable

--- northwind.rb
+++ northwind.rb
@@ -2,7 +2,7 @@ require 'active_record'
 require 'yaml'
 require 'logger'
 
-dbconfig = YAML::load(File.open('/home/lym/ruby_projects/northwind/database.yml'))
+dbconfig = YAML::load(File.open(ENV["HOME"] + '/ruby_projects/northwind/database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig)
 ActiveRecord::Base.logger = Logger.new(STDERR)
 
